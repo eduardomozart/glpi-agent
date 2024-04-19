f2ad12bee7c11d3c0b2bc954313c36fa4b00b3a4
Eduardo Mozart de Oliveira <2974895+eduardomozart@users.noreply.github.com>
2024-04-18 22:43:10 -0300

    Update Ubnt.pm
    Fix Serial Number (UniFi AP's aren't imported by default as they don't have one assigned) and improve Firmware and Model detection of UniFi AP's.

--- lib/GLPI/Agent/SNMP/MibSupport/Ubnt.pm
+++ lib/GLPI/Agent/SNMP/MibSupport/Ubnt.pm
@@ -10,20 +10,37 @@ use GLPI::Agent::Tools::SNMP;
 
 # See UBNT-MIB
 
-use constant ubnt               => '.1.3.6.1.4.1.41112';
-use constant ubntWlStatApMac    => ubnt . '.1.4.5.1.4.1';
+use constant ubnt              => '.1.3.6.1.4.1.41112';
+use constant ubntWlStatApMac   => ubnt . '.1.4.5.1.4.1';
+
+# See UBNT-UniFi-MIB
+
+use constant unifiApSystemVersion => ubnt . '.1.6.3.6.0';
+use constant unifiApSystemModel	  => ubnt . '.1.6.3.3.0';
 
 our $mibSupport = [
     {
         name    => "ubnt",
-        oid     => ubnt
+        sysobjectid => getRegexpOidMatch(ubnt)
     }
 ];
+
+sub getFirmware {
+    my ($self) = @_;
+
+    return getCanonicalString($self->get(unifiApSystemVersion));
+}
+
+sub getModel {
+    my ($self) = @_;
+
+    return getCanonicalString($self->get(unifiApSystemModel));
+}
 
 sub getSerial {
     my ($self) = @_;
 
-    my $serial = getCanonicalMacAddress($self->get(ubntWlStatApMac));
+    my $serial = $self->getMacAddress;
     $serial =~ s/://g;
 
     return $serial;
@@ -31,8 +48,15 @@ sub getSerial {
 
 sub getMacAddress {
     my ($self) = @_;
+	
+	my $device = $self->device
+        or return;
+	
+	my $serial = getCanonicalMacAddress($self->get(ubntWlStatApMac));
+	$serial = $device->{MAC}
+				if not defined ($serial);
 
-    return $self->get(ubntWlStatApMac);
+    return $serial;
 }
 
 1;
